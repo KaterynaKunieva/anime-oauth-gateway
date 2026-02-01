68566e4306b32c6bc2441b4eab52681f9508105a
Kateryna <kunieva.kateryna@gmail.com>
2026-02-01 13:22:52 +0100

    Initial commit

---
+++ src/main/java/org/kunievakateryna/service/UnauthorizedException.java
@@ -0,0 +1,9 @@
+package org.kunievakateryna.service;
+
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+    
+}
